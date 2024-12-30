96dda785beaa21495352532f052585fab4ae4c0d
Aishik Dasgupta <itsdg19@gmail.com>
2024-12-30 15:45:05 +0530

    temp homepage update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
-  )
-}
+    <div className="h-screen flex items-center justify-center font-bold">
+      SchoolGO - school managment systems by ServiceGO{" "}
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
